f55a72a26f9f4778f43e247588cfbc19847a4a04
fj0lverkr <nils.nahooy@live.com>
2020-08-17 14:17:04 +0200

    replaced var with const.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(function(){
   // adjust navbar when document is scrolled
   $(document).scroll(
     function(){
-      var $nav = $("#main-nav");
+      const $nav = $("#main-nav");
       $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height()  && $(this).scrollTop() < $(document).height() - $(window).height() - 100);
     });
 });
